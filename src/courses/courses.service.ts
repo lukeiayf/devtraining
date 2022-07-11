d5eb7833148ea361d21fcf9e7ae3cbddc2c8ca67
Lucas Silva <lucas.silva@tsaaviacao.com.br>
2022-07-11 10:20:59 -0300

    reinitiate repository

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,45 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { response } from 'express';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [{
+    id: 1,
+    name: "Fundamentos do framework Nestjs",
+    description: "Fundamentos do framework Nestjs",
+    tags: ['node.js', 'nest.js', 'javascript'],
+  }];
+
+  findAll(){
+    return this.courses;
+  }
+
+  findOne(id:string){
+    const course = this.courses.find((course) => course.id === Number(id));
+
+    if (!course) {
+      throw new HttpException(`Course ID ${id} not found`, HttpStatus.NOT_FOUND)
+    }
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any){
+    const indexCourse = this.courses.findIndex((course: Course) => course.id === Number(id));
+
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string){
+    const indexCourse = this.courses.findIndex((course: Course) => course.id === Number(id));
+
+    if(indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1);
+    }
+  }
+}
+
+
